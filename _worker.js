247fa5d2f07092c789af7eb1dd61a7eef067b316
Ko Latt <mythicko@gmail.com>
2024-07-15 00:08:45 +0630

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,59 +1,59 @@
 /**
-*- Telegram交流群：https://t.me/AM_CLUBS
-*- YouTube频道：https://youtube.com/@AM_CLUB
-*- VLESS临时订阅地址：https://worker.amcloud.filegear-sg.me/866853eb-5293-4f09-bf00-e13eb237c655
-*- Github仓库地址：https://github.com/ansoncloud8
+*- Telegram group: https://t.me/AM_CLUBS
+*- YouTube channel: https://youtube.com/@AM_CLUB
+*- VLESS temporary subscription address: https://worker.amcloud.filegear-sg.me/866853eb-5293-4f09-bf00-e13eb237c655
+*- Github repository address: https://github.com/ansoncloud8
 **/
-// 定义一个名为 mytoken 的变量，并将 'passwd' 作为默认的读写权限
+// Define a variable called mytoken and set 'passwd' as the default read and write permissions
 let mytoken= 'passwd';
 
 export default {
 	async fetch (request, env) {
-		// 如果环境变量中有 TOKEN，则将其赋值给 mytoken，否则保持默认值
+		// If there is a TOKEN in the environment variable, assign it to mytoken, otherwise keep the default value
 		mytoken = env.TOKEN || mytoken;
 
 		let KV;
-		// 检查 KV (键值存储) 是否已经被设置
+		// Check if KV (key-value storage) has been set
 		if (env.KV) {
-			// 将 env.KV 赋值一个名为 KV 的常量
+			// Assign env.KV to a constant named KV
 			KV =  env.KV;
 		} else {
-			//throw new Error('KV 命名空间未绑定');
-			return new Response('KV 命名空间未绑定', {
+			//throw new Error('KV namespace is not bound');
+			return new Response('KV namespace not bound', {
 				status: 400,
 				headers: { 'content-type': 'text/plain; charset=utf-8' },
 			});
 		}
 
-		// 从请求的 URL 中获取需要的参数
+		// Get the required parameters from the requested URL
 		const url = new URL(request.url);
 		let token;
 		if (url.pathname === `/${mytoken}`){
 			token = mytoken;
 		} else {
-			// 获取 URL 查询参数中的 'token'，如果不存在则赋值为 "null"
+			// Get the 'token' in the URL query parameter, if it does not exist, assign it to "null"
 			token = url.searchParams.get('token') || "null";
 		}
 
-		// 检查提供的 token 是否与 mytoken 相符
+		// Check if the provided token matches mytoken
 		if (token === mytoken) {
 			const 文件名 = url.pathname.startsWith('/') ? url.pathname.substring(1) : url.pathname;
 
-			if (文件名 == "config" || 文件名 == mytoken) {
+			if (filename == "config" || filename == mytoken) {
 				const html = configHTML(url.hostname, token);
 				return new Response(html, {
 				  headers: {
 					'Content-Type': 'text/html; charset=UTF-8',
 				  },
 				});
-			} else if (文件名 == "config/update.bat") {
+			} else if (filename == "config/update.bat") {
 				return new Response(下载bat(url.hostname, token), {
 				  headers: {
 					"Content-Disposition": `attachment; filename=update.bat`, 
 					"content-type": "text/plain; charset=utf-8",
 				  },
 				});
-			} else if (文件名 == "config/update.sh") {
+			} else if (filename == "config/update.sh") {
 				return new Response(下载sh(url.hostname, token), {
 				  headers: {
 					"Content-Disposition": `attachment; filename=update.sh`, 
@@ -61,31 +61,31 @@ export default {
 				  },
 				});
 			} else {
-				// 获取 URL 查询参数中的 'text' 和 'b64'，如果不存在则赋值为 "null"
+				// Get the 'text' and 'b64' in the URL query parameters, and set them to "null" if they do not exist
 				const text = url.searchParams.get('text') || "null";
 				const b64 = url.searchParams.get('b64') || "null";
 
-				// 如果 'text' 和 'b64' 都为 "null"，则从 KV 中读取并返回文件内容
+				// If both 'text' and 'b64' are "null", read and return the file contents from KV
 				if (text === "null" && b64 === "null"){
-					const value = await KV.get(文件名);
+					const value = await KV.get(filename);
 					return new Response(value , {
 						status: 200,
 						headers: { 'content-type': 'text/plain; charset=utf-8' },
 					});
 				} else {
-					// 检查文件是否存在
-					await fileExists(KV, 文件名);
+					// Check if the file exists
+					await fileExists(KV, filename);
 					
-					// 如果 'b64' 为 "null" ，则以明文方式写入文件，如果 'text' 为 "null" ，则以 base64 方式写入文件
+					// If 'b64' is "null", write the file in plain text; if 'text' is "null", write the file in base64
 					if (b64 === "null" ){
-						await KV.put(文件名, text);
+						await KV.put(filename, text);
 						return new Response(text, {
 							status: 200,
 							headers: { 'content-type': 'text/plain; charset=utf-8' },
 						});
 					} else if (text === "null" ){
-						await KV.put(文件名, base64Decode(空格替换加号(b64)));
-						return new Response(base64Decode(空格替换加号(b64)), {
+						await KV.put(filename, base64Decode(replace spaces with plus signs(b64)));
+						return new Response(base64Decode(replace the space with the plus sign(b64)), {
 							status: 200,
 							headers: { 'content-type': 'text/plain; charset=utf-8' },
 						});
@@ -94,7 +94,7 @@ export default {
 			}
 
 			
-		} else if (url.pathname == "/"){//首页改成一个nginx伪装页
+		} else if (url.pathname == "/"){//The homepage is changed to an nginx disguise page
 			return new Response(`
 			<!DOCTYPE html>
 			<html>
@@ -126,8 +126,8 @@ export default {
 				'Content-Type': 'text/html; charset=UTF-8',
 			  },
 			});
-		} else {// 如果 token 不符，返回 'token 有误'//
-			return new Response('token 有误', {
+		} else {// If the token does not match, return 'token error'//
+			return new Response('token error', {
 				status: 400,
 				headers: { 'content-type': 'text/plain; charset=utf-8' },
 			});
@@ -135,103 +135,103 @@ export default {
 	}
 };
 
-// 定义一个名为 fileExists 的异步函数，通过查询 KV 中是否有 filename 对应的值来判断文件是否存在
+// Define an asynchronous function named fileExists to check whether the file exists by checking whether there is a value corresponding to filename in KV
 async function fileExists(KV, filename) {
 	const value = await KV.get(filename);
 	return value !== null;
 }
 
-// 定义一个名为 base64Decode 的函数，用于将 base64 编码的字符串转换为 utf-8 编码的字符
+// Define a function called base64Decode to convert base64-encoded strings to utf-8-encoded characters
 function base64Decode(str) {
 	const bytes = new Uint8Array(atob(str).split('').map(c => c.charCodeAt(0)));
 	const decoder = new TextDecoder('utf-8');
 	return decoder.decode(bytes);
 }
 
-function 空格替换加号(str) {
+function space-replace-plus-sign(str) {
 	str = str.replace(/ /g, '+');
 	return str;
 }
 
-function 下载bat(域名,token) {
+function downloadbat(domain name, token) {
 	return [
 	  `@echo off`,
 	  `chcp 65001`,
 	  `setlocal`,
 	  ``,
-	  `set "DOMAIN=${域名}"`,
+	  `set "DOMAIN=${domain name}"`,
 	  `set "TOKEN=${token}"`,
 	  ``,
-	  `rem %~nx1表示第一个参数的文件名和扩展名`,
+	  `rem %~nx1 represents the file name and extension of the first parameter`,
 	  `set "FILENAME=%~nx1"`,
 	  ``,
-	  `rem PowerShell命令读取文件的前65行内容，将内容转换为UTF8并进行base64编码`,
+	  `rem PowerShell command reads the first 65 lines of the file, converts the content to UTF8 and encodes it in base64`,
 	  `for /f "delims=" %%i in ('powershell -command "$content = ((Get-Content -Path '%cd%/%FILENAME%' -Encoding UTF8) | Select-Object -First 65) -join [Environment]::NewLine; [convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($content))"') do set "BASE64_TEXT=%%i"`,
 	  ``,
-	  `rem 将内容保存到response.txt`,
+	  `rem save the content to response.txt`,
 	  `rem echo %BASE64_TEXT% > response.txt`,
 	  ``,
-	  `rem 构造带有文件名和内容作为参数的URL`,
+	  `rem constructs a URL with filename and content as parameters`,
 	  `set "URL=https://%DOMAIN%/%FILENAME%?token=%TOKEN%^&b64=%BASE64_TEXT%"`,
 	  ``,
-	  `rem 显示请求的响应 `,
+	  `rem shows the response to the request`,
 	  `rem powershell -Command "(Invoke-WebRequest -Uri '%URL%').Content"`,
 	  `start %URL%`,
 	  `endlocal`,
 	  ``,
-	  `echo 更新数据完成,倒数5秒后自动关闭窗口...`,
+	  `echo update data completed, automatically close the window after 5 seconds countdown...`,
 	  `timeout /t 5 >nul`,
 	  `exit`
 	].join('\r\n');
 }
 
-function 下载sh(域名,token) {
+function downloadsh(domain name, token) {
 	return `#!/bin/bash
 export LANG=zh_CN.UTF-8
-DOMAIN="${域名}"
+DOMAIN="${domain name}"
 TOKEN="${token}"
 if [ -n "$1" ]; then 
   FILENAME="$1"
 else
-  echo "无文件名"
+  echo "no file name"
   exit 1
-fi
+be
 BASE64_TEXT=$(head -n 65 $FILENAME | base64 -w 0)
 curl -k "https://$DOMAIN/$FILENAME?token=$TOKEN&b64=$BASE64_TEXT"
-echo "更新数据完成"
+echo "Update data completed"
 `
 }
 
-function configHTML(域名, token) {
+function configHTML(domain name, token) {
 	return `
 	  <html>
 		<head>
 		  <title>CF-Workers-TEXT2KV</title>
 		</head>
 		<body>
-			<p align='center'><img src='https://ansoncloud8.github.io/logo.png' alt='图片描述' style='margin-bottom: 20px;'>
-			<b style='font-size: 15px;'>欢迎如果您发现这个项目很好用，请查看我们的 GitHub 项目给我一个star：</b>
+			<p align='center'><img src='https://ansoncloud8.github.io/logo.png' alt='Image Description' style='margin-bottom: 20px;'>
+			<b style='font-size: 15px;'>If you find this project useful, please check out our GitHub project and give me a star:</b>
 			<a href='https://github.com/ansoncloud8/am-tunnel' target='_blank'>am-tunnel</a>
 			<iframe src='https://ghbtns.com/github-btn.html?user=ansoncloud8&repo=am-tunnel&type=star&count=true&size=large' frameborder='0' scrolling='0' width='170' height='30' title='GitHub'></iframe>
 	
-		  <h1 class="centered">CF-Workers-TEXT2KV 配置信息</h1>
+		  <h1 class="centered">CF-Workers-TEXT2KV configuration information</h1>
 		  <p class="centered">
-		  服务域名: ${域名} <br>
+		  Service domain name: ${domain name} <br>
 		  token: ${token} <br>
 		  <br>
-		  <pre>注意! 因URL长度内容所限，脚本更新方式一次最多更新65行内容</pre><br>
-		  Windows脚本: <button type="button" onclick="window.open('https://${域名}/config/update.bat?token=${token}', '_blank')">点击下载</button>
+		  <pre>Note! Due to the limited length of the URL, the script update method can update a maximum of 65 lines of content at a time</pre><br>
+		  Windows script: <button type="button" onclick="window.open('https://${domain name}/config/update.bat?token=${token}', '_blank')">Click to download</button>
 		  <br>
 		  <pre>使用方法: <code>&lt;update.bat&nbsp;ip.txt&gt;</code></pre>
 		  <br>
-		  Linux脚本: 
+		  Linux script:
 		  <code>&lt;curl&nbsp;https://${域名}/config/update.sh?token=${token}&nbsp;-o&nbsp;update.sh&nbsp;&&&nbsp;chmod&nbsp;+x&nbsp;update.sh&gt;</code><br>
 		  <pre>使用方法: <code>&lt;./update.sh&nbsp;ip.txt&gt;</code></pre><br>
 		  <br>
-		  在线文档查询: <br>
-		  https://${域名}/<input type="text" name="keyword" placeholder="请输入要查询的文档">?token=${token}    
-		  <button type="button" onclick="window.open('https://${域名}/' + document.querySelector('input[name=keyword]').value + '?token=${token}', '_blank')">查看文档内容</button>
-		  <button type="button" onclick="navigator.clipboard.writeText('https://${域名}/' + document.querySelector('input[name=keyword]').value + '?token=${token}')">复制文档地址</button>
+		  Online Documentation Search: <br>
+		  https://${domain name}/<input type="text" name="keyword" placeholder="Please enter the document to be queried">?token=${token}    
+		  <button type="button" onclick="window.open('https://${domain name}/' + document.querySelector('input[name=keyword]').value + '?token=${token}', '_blank')">View document content</button>
+		  <button type="button" onclick="navigator.clipboard.writeText('https://${domain name}/' + document.querySelector('input[name=keyword]').value + '?token=${token}')">Copy document address</button>
 		  </p>
 	  <br>
 		</body>
